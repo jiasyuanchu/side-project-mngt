b4622bf45116c996dff5b4a598e22c65fc994961
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-15 15:34:07 +0800

    feat: add views

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express')
-const handlebars = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
-app.engine('hbs', handlebars({ extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('this is home page.')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
-
 
 module.exports = app

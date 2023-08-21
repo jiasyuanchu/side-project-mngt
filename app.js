e04102dad87d0a15a012521ad2b4a15ae9141ade
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-21 17:57:25 +0800

    fix

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const exphbs = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs' })
+app.engine('hbs', hbs.engine)
+
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
